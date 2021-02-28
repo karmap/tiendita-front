a3d730ebc4e047ae77e782889f522866e2f712d9
anaesc <virgo.esc@gmail.com>
2021-02-27 22:07:23 -0600

    cambié hello world por hola mundo

--- pages/unirse.js
+++ pages/unirse.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function IndexPage() {
   return (
     <div>
-      Hello World.{' '}
+      Hola Mundo.{' '}
       <Link href="/registro">
         <a>ir al registro</a>
       </Link>
